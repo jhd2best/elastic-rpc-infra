5ebca78ee74fb9c83b873966be778eacab2482c9
Diego Nava <diego.nava77@hotmail.com>
2022-07-19 12:20:31 +0200

    chore: adjust redis cluster

--- terraform/mainet/eu/main.tf
+++ terraform/mainet/eu/main.tf
@@ -51,13 +51,13 @@ module "elastic" {
   redis_version = "redis6.x"
   shard_conf = [{
     shard_number                  = 0
-    redis_shards                  = 3
+    redis_shards                  = 2
     redis_replicas_per_node_group = 1
     redis_instance_type           = "cache.r6g.large"
     },
     {
       shard_number                  = 1
-      redis_shards                  = 3
+      redis_shards                  = 1
       redis_replicas_per_node_group = 1
       redis_instance_type           = "cache.r6g.large"
   }]
