3d6860dbe548b75ff396d86b04ab22a733b95ec3
Diego Nava <diego.nava77@hotmail.com>
2022-08-23 13:49:53 +0200

    feat: add health checks

--- terraform/mainet/route53.tf
+++ terraform/mainet/route53.tf
@@ -24,7 +24,8 @@ resource "aws_route53_record" "global_api" {
   ttl      = 300
   records  = ["api.erpc-${split("-", each.key)[0]}.s${split("-", each.key)[1]}.${local.domain}"]
 
-  set_identifier = "api-${split("-", each.key)[0]}.s${split("-", each.key)[1]}"
+  set_identifier  = "api-${split("-", each.key)[0]}.s${split("-", each.key)[1]}"
+  health_check_id = aws_route53_health_check.this[each.key].id
 
   latency_routing_policy {
     region = local.regions_to_aws[split("-", each.key)[0]]
@@ -39,9 +40,26 @@ resource "aws_route53_record" "global_ws" {
   ttl      = 300
   records  = ["ws.erpc-${split("-", each.key)[0]}.s${split("-", each.key)[1]}.${local.domain}"]
 
-  set_identifier = "ws-${split("-", each.key)[0]}.s${split("-", each.key)[1]}"
+  set_identifier  = "ws-${split("-", each.key)[0]}.s${split("-", each.key)[1]}"
+  health_check_id = aws_route53_health_check.this[each.key].id
 
   latency_routing_policy {
     region = local.regions_to_aws[split("-", each.key)[0]]
+  }
+}
+
+resource "aws_route53_health_check" "this" {
+  for_each          = local.global_elbs
+  failure_threshold = "10"
+  fqdn              = "lb.${split("-", each.key)[0]}.main.t.hmny.io"
+  request_interval  = "30"
+  resource_path     = "/s${split("-", each.key)[1]}/writer/explorer/node-sync"
+  search_string     = "true"
+  port              = 443
+  type              = "HTTPS_STR_MATCH"
+  measure_latency   = true
+
+  tags = {
+    Name = "erpc-${split("-", each.key)[0]}-s${split("-", each.key)[1]}"
   }
 }
